970bba1235386362ec7f93c049c8ff406ba1414a
Keerthan0811 <158686428+Keerthan0811@users.noreply.github.com>
2024-07-17 19:14:11 +0530

    Update Brute_force1.c

--- Brute_force1.c
+++ Brute_force1.c
@@ -1,4 +1,4 @@
-// 268. Missing Number
+// 328. Odd Even Linked List
 // Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
 // The first node is considered odd, and the second node is even, and so on.
 // Note that the relative order inside both the even and odd groups should remain as it was in the input.
